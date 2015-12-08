21550ef0e4163284bf1206551bbbe487b93e4ded
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2015-12-08 14:06:26 +0100

    Added mailer deferred email processor

---
+++ server_guardian_api/processors/__init__.py
@@ -0,0 +1,1 @@
+"""A collection of processors, pre-made to be used with the guardian API."""
--- server_guardian_api/processors/debug.py
+++ server_guardian_api/processors/debug.py
@@ -1,6 +1,6 @@
-"""Some common processors."""
+"""Some processors for debugging and testing."""
 
-from .constants import SERVER_STATUS
+from ..constants import SERVER_STATUS
 
 
 def exception_processor():  # pragma: no cover
@@ -11,7 +11,7 @@ def exception_processor():  # pragma: no cover
 def dummy_processor():  # pragma: no cover
     """Use this processor to test, that you've set up everything correct."""
     return {
-        'label': 'dummy',
+        'label': 'Dummy',
         'status': SERVER_STATUS['OK'],
         'info': 'This is a dummy response.'
     }
