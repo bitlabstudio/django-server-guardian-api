71f938b60fac492a909aa097e52772166d180656
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2015-12-08 13:34:26 +0100

    Added API URLs

--- server_guardian_api/urls.py
+++ server_guardian_api/urls.py
@@ -1,12 +1,12 @@
 """URLs for the server_guardian_api app."""
-# from compat import patterns, url
+from compat import patterns, url
 
-# from . import views
+from . import views
 
 
-# urlpatterns = patterns(
-#     '',
-#     url(r'^$',
-#         views.YourView.as_view(),
-#         name='server_guardian_api_default'),
-# )
+urlpatterns = patterns(
+    '',
+    url(r'^$',
+        views.ServerGuardianAPIView.as_view(),
+        name='server_guardian_api'),
+)
