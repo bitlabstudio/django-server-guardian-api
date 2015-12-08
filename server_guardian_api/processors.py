409a109b6612d0a239324da2ef69c0b79a17455f
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2015-12-08 12:45:22 +0100

    Added test processors

---
+++ server_guardian_api/processors.py
@@ -0,0 +1,16 @@
+"""Some common processors."""
+
+from .constants import SERVER_STATUS
+
+
+def exception_processor():  # pragma: no cover
+    """This processor is only useful to test."""
+    raise Exception('Something went wrong.')
+
+
+def dummy_processor():  # pragma: no cover
+    """Use this processor to test, that you've set up everything correct."""
+    return {
+        'status': SERVER_STATUS['OK'],
+        'info': 'This is a dummy response.'
+    }
